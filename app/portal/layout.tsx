48e34134ffc0f60e2b3f950d9b648a127322dc65
nmiller3300 <nmiller3300@gmail.com>
2026-08-21 11:25:35 -0400

    Load Command Structure preview styles

--- app/portal/layout.tsx
+++ app/portal/layout.tsx
@@ -7,6 +7,7 @@ import "./command-v2.css";
 import "./quick-find.css";
 import "./home-v2.css";
 import "./supervision-v2.css";
+import "./structure-v2.css";
 
 export const metadata: Metadata = {
   title: "Personnel Operations Portal",
