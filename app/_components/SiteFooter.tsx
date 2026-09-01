75899ee579d3b1eca22981525d763ad386a92438
nmiller3300 <nmiller3300@gmail.com>
2026-08-31 23:04:07 -0400

    Remove public policy directory link

--- app/_components/SiteFooter.tsx
+++ app/_components/SiteFooter.tsx
@@ -25,7 +25,6 @@ export function SiteFooter() {
             <Link href="/about">About LSCSO</Link>
             <Link href="/office-of-the-sheriff">Office of the Sheriff</Link>
             <Link href="/internal-affairs">Internal Affairs</Link>
-            <Link href="/policies">Policy Directory</Link>
             <Link href="/portal">Personnel Portal</Link>
           </div>
           <div>
