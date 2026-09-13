14bfccfa87d4004e0f65d844aa6f8961eb226c9e
nmiller3300 <nmiller3300@gmail.com>
2026-09-13 15:29:34 -0400

    Restore archive stylesheet baseline

--- app/archives/layout.tsx
+++ app/archives/layout.tsx
@@ -1,9 +1,6 @@
 import type { ReactNode } from "react";
 import "./archive-safari-fix.css";
 import "./archive-folder-fan.css";
-import "./archive-cross-platform.css";
-import "./archive-folder-hitbox-fix.css";
-import "./archive-lamp-regression-fix.css";
 
 export default function ArchivesLayout({ children }: { children: ReactNode }) {
   return children;
