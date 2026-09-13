b4fd19ee2b9bbcf393a4bf2b49a3eb62d74d16e0
nmiller3300 <nmiller3300@gmail.com>
2026-09-13 13:57:21 -0400

    Load physical archive folder interaction

--- app/archives/layout.tsx
+++ app/archives/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./archive-safari-fix.css";
+import "./archive-folder-fan.css";
 
 export default function ArchivesLayout({ children }: { children: ReactNode }) {
   return children;
