0c9a979f35cff8eb5c82ff1fb357ceb2666f3ea3
nmiller3300 <nmiller3300@gmail.com>
2026-09-13 15:09:32 -0400

    Load archive folder hitbox hardening

--- app/archives/layout.tsx
+++ app/archives/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import "./archive-safari-fix.css";
 import "./archive-folder-fan.css";
 import "./archive-cross-platform.css";
+import "./archive-folder-hitbox-fix.css";
 
 export default function ArchivesLayout({ children }: { children: ReactNode }) {
   return children;
