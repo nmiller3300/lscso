e38e0ddaa6c0514025eb405baec96a4856c87d30
nmiller3300 <nmiller3300@gmail.com>
2026-09-13 15:13:34 -0400

    Load archive lamp regression fix

--- app/archives/layout.tsx
+++ app/archives/layout.tsx
@@ -3,6 +3,7 @@ import "./archive-safari-fix.css";
 import "./archive-folder-fan.css";
 import "./archive-cross-platform.css";
 import "./archive-folder-hitbox-fix.css";
+import "./archive-lamp-regression-fix.css";
 
 export default function ArchivesLayout({ children }: { children: ReactNode }) {
   return children;
