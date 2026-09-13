a6bd3dff11cbea827af3e68787f7ae536cdad116
nmiller3300 <nmiller3300@gmail.com>
2026-09-13 15:38:00 -0400

    Load isolated archive folder motion

--- app/archives/layout.tsx
+++ app/archives/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import "./archive-safari-fix.css";
 import "./archive-folder-fan.css";
 import "./archive-folder-hitbox-fix.css";
+import "./archive-folder-motion.css";
 
 export default function ArchivesLayout({ children }: { children: ReactNode }) {
   return children;
