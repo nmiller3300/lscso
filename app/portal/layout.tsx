bfc61239fe66c09df3c40c777816f328fbe8858d
nmiller3300 <nmiller3300@gmail.com>
2026-09-13 11:49:06 -0400

    Load final glowing login polish

--- app/portal/layout.tsx
+++ app/portal/layout.tsx
@@ -31,6 +31,7 @@ import "./personnel-directory-exports.css";
 import "./portal-ui-upgrades.css";
 import "./portal-ui-upgrades-2.css";
 import "./portal-glow-login.css";
+import "./portal-glow-login-polish.css";
 
 export const metadata: Metadata = {
   title: "Personnel Operations Portal",
