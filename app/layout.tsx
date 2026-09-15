2944e9594c4bc00e88b91d7d1549f0a7a363507d
nmiller3300 <nmiller3300@gmail.com>
2026-09-15 14:36:42 -0400

    Load cinematic public workflow styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./public-premium-system.css";
 import "./public-header-glass-restore.css";
 import "./public-cinematic-system.css";
 import "./public-cinematic-pages.css";
+import "./public-cinematic-workflows.css";
 import "./maintenance.css";
 
 const geist = localFont({
